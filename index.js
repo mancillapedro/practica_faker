27819b2ed6e2eae95178d8efef0edb0af46a85c0
_ <pedropmancillac@gmail.com>
2023-06-17 13:47:31 -0400

    doc modulo11

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ const {
     format,
     // getCheckDigit
 } = require('rut.js');
-const CURRENT_DATE = new Date();
 
+/**
+ * @description https://es.wikipedia.org/wiki/Rol_%C3%9Anico_Tributario
+ * @param {Number} rut_cuerpo 
+ * @returns {String}
+ */
 const modulo11 = (rut_cuerpo) => {
-    // https://es.wikipedia.org/wiki/Rol_%C3%9Anico_Tributario
     const
         serie = [2, 3, 4, 5, 6, 7],
         digit = 11 - (String(rut_cuerpo).split("").reverse().reduce(
             (acc, digit, index) => acc + (digit * serie[index % 6]), 0) % 11)
-    return digit === 11 ? 0 : digit === 10 ? "K" : String(digit)
+    return digit === 11 ? '0' : digit === 10 ? "K" : String(digit)
 }
 
 const getRut = () => {
     const
         MIN = 1_000_000,
         MAX = 30_999_999,
-        randomNumber = String(Math.floor(Math.random() * (MAX - MIN + 1) + MIN))
+        randomNumber = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
     // return format(randomNumber + getCheckDigit(randomNumber))
     return format(randomNumber + modulo11(randomNumber))
 }
 
 const getAge = birthDate => {
     const
+        CURRENT_DATE = new Date(),
         years = CURRENT_DATE.getFullYear() - birthDate.getFullYear(),
         months = CURRENT_DATE.getMonth() - birthDate.getMonth()
     return months < 0 || (months === 0 && CURRENT_DATE.getDate() < birthDate.getDate())
