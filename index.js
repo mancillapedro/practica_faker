afc911773f97fb89a9a732c449680d503dc4797b
_ <pedropmancillac@gmail.com>
2023-06-19 22:56:29 -0400

    consoles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Person } = require('./Person.js');
 
-const personsInfo = persons_array => persons_array.forEach(person => console.log(person.info))
+const arrayOfPersons = amount => Array.from({ length: amount }, () => new Person());
+
+const personsInfo = persons_array => persons_array.map(person => person.info)
 
 const personsByCities = persons_array => Object
     .entries(
@@ -16,22 +18,17 @@ const personsByCities = persons_array => Object
         return acc
     }, {})
 
-
 const averageAgeOfPersons = persons_array =>
     persons_array.reduce((total, person) => total + person.age, 0) / persons_array.length
-
 
 const personsBySex = persons_array => persons_array.reduce((acc, person) => {
     acc[person.sex] ? (acc[person.sex]++) : (acc[person.sex] = 1)
     return acc
 }, {})
 
-
-const arrayOfPersons = amount => Array.from({ length: amount }, () => new Person());
-
 const persons = arrayOfPersons(10);
 
-personsInfo(persons)
-console.log(personsByCities(persons));
-console.log(averageAgeOfPersons(persons));
-console.log(personsBySex(persons));
+console.log("Listado de personas", personsInfo(persons))
+console.log("\nPersonas por ciudades", personsByCities(persons));
+console.log("\nPromedio de edades", averageAgeOfPersons(persons));
+console.log("\nPersonas por cada sexo", personsBySex(persons));
